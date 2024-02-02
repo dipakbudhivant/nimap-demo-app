368359bac555bd0b7a0d3e6d5d37d5175f0dc296
dipakbudhivant <48544836+dipakbudhivant@users.noreply.github.com>
2024-02-02 12:53:54 +0530

    Update profile.component.ts
    Edit Func

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,18 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormDataServiceService } from '../../form-data-service.service';
+import { RegistrationPopupComponent } from '../home/registration-popup/registration-popup.component';
+import { NbWindowService } from '@nebular/theme';
 
 @Component({
   selector: 'ngx-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
-  constructor(private formDataService: FormDataServiceService) {}
+export class ProfileComponent implements OnInit, AfterViewInit {
+  constructor(private formDataService: FormDataServiceService, private windowService: NbWindowService) {}
   formData: any;
   ngOnInit() {
     console.log('first');
     this.formData =  this.formDataService.getFormData();
     console.log('Form Data in Success Component:', this.formData);
 
+  }
+  ngAfterViewInit(){
+    this.formData =  this.formDataService.getFormData();
+    console.log('Form Data in Success Component:', this.formData);
+  }
+  editForm(){
+    console.log("Edit Form")
+    this.windowService.open(
+      RegistrationPopupComponent,
+      {
+        title: 'Edit Profile',
+        hasBackdrop: false,
+        closeOnEsc: false,
+        context: {
+          context: this.formData,
+          assignFormDataCallback: this.assignFormData.bind(this),  
+        },    
+      },
+    );
+  }
+
+  assignFormData(){
+    this.formData =  this.formDataService.getFormData();
+    console.log('Form Data in Success Component:', this.formData);
   }
 }
